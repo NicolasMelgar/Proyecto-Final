11f08b45482176892aed46cfd870ab027c68b3b2
Nicolas Melgar <elnico521@hotmail.com>
2023-06-10 08:26:21 -0300

    Ajustando form php

--- form.php
+++ form.php
@@ -1,13 +1,13 @@
 <?php   
 $nombre = $_POST['nombre'];
 $mail = $_POST['email']; 
-$mensaje = $_POST['mensaje'];
+$mensaje = $_POST['textArea'];
 
 // $header .= "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
 $mensaje .= "Su mail es " . $mail . "\r\n";
-$mensaje .= "Su mensaje es " . $_POST['mensaje'] . "\r\n";
+$mensaje .= "Su mensaje es " . $_POST['textArea'] . "\r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
 $para = 'nicolasmelgarparadizo@gmail.com'; //el mail a donde van a llegar los mensajes
