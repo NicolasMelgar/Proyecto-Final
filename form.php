20b325234be993b8d862c72024ed2341fc571e6d
Nicolas Melgar <elnico521@hotmail.com>
2023-06-10 11:55:19 -0300

    submit listo pero sin PHP

--- form.php
+++ form.php
@@ -3,14 +3,14 @@ $nombre = $_POST['nombre'];
 $mail = $_POST['email']; 
 $mensaje = $_POST['textArea'];
 
-// $header .= "Content-Type: text/plain";
+$header .= "Content-Type: text/plain";
 
-$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje = "Este mensaje fue enviado por " . $nombre . "\r\n";
 $mensaje .= "Su mail es " . $mail . "\r\n";
 $mensaje .= "Su mensaje es " . $_POST['textArea'] . "\r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'nicolasmelgarparadizo@gmail.com'; //el mail a donde van a llegar los mensajes
+$para = 'elnico521@hotmail.com'; //el mail a donde van a llegar los mensajes
 $asunto = 'Mensaje de mi Sitio Web'; //El asunto del mensaje que me llegará
 
 mail($para, $asunto, utf8_decode($mensaje), $header); 
